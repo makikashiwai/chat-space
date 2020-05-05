ada17cd4605af43c1bb3e326befd1bd63c75942f
makikashiwai <mmmkpn@gmail.com>
2020-05-05 16:13:49 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(function(){ 
   function buildHTML(message){
-    if ( message.image ) {
+    if ( message.content && message.image ) {
       var html =
-        `<div class="main-chat__message-list__massage">
+        `<div class="main-chat__message-list__massage" data-message-id=${message.id}>
           <div class="main-chat__message-list__massage__upper-info">
             <p class="main-chat__message-list__massage__upper-info__talker">
               ${message.user_name}
@@ -14,11 +14,11 @@ $(function(){
           <p class="main-chat__message-list__massage__text">
             ${message.content}
           </p>
-          <img src=${message.image} >
+          <img class="lower-message__image" src="${message.image}">
         </div>`
-    } else {
+    } else if (message.content){
       var html =
-        `<div class="main-chat__message-list__massage">
+        `<div class="main-chat__message-list__massage" data-message-id=${message.id}>
           <div class="main-chat__message-list__massage__upper-info">
             <p class="main-chat__message-list__massage__upper-info__talker">
               ${message.user_name}
@@ -31,8 +31,22 @@ $(function(){
             ${message.content}
           </p> 
         </div>`
-      return html;
+    }else if (message.image){
+      var html = `<div class="main-chat__message-list__massage" data-message-id="${message.id}">
+      <div class="main-chat__message-list__massage__upper-info">
+      <p class="main-chat__message-list__massage__upper-info__talker">
+      ${message.user_name}
+      </p>
+      <p class="main-chat__message-list__massage__upper-info__date">
+      ${message.created_at}
+      </p>
+      </div>
+      <p class="main-chat__message-list__massage__text">
+      <img class="lower-message__image" src="${message.image}" alt="Eunfllfucayj17z">
+      </p>
+      </div>`
     };
+    return html;
   }
 $('#new_message').on('submit', function(e){
   e.preventDefault();
@@ -57,4 +71,31 @@ $('#new_message').on('submit', function(e){
     alert("メッセージ送信に失敗しました");
   });
 })
+  var reloadMessages = function() {
+    var last_message_id = $('.main-chat__message-list__massage:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-chat__message-list').append(insertHTML);
+        $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".submit-btn").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
